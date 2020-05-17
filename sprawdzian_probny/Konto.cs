3dbc4d835d6a40ab9ca2e8091cee976f44de6a65
kowalskabe <58075828+kowalskabe@users.noreply.github.com>
2020-05-17 18:40:55 +0200

    Konto i KontoFirmowe

--- sprawdzian_probny/Konto.cs
+++ sprawdzian_probny/Konto.cs
@@ -8,5 +8,36 @@ namespace sprawdzian_probny
 {
     class Konto
     {
-    }
+		private string wlasciciel;
+		private int saldo;
+
+		public string Wlasciciel
+		{
+			get { return wlasciciel; }
+			set { wlasciciel = value; }
+		}
+
+		public int Saldo
+		{
+			get { return saldo; }
+			set { saldo = value; }
+		}
+
+
+		public Konto(string wlasciciel, int saldo)
+		{
+			this.wlasciciel = wlasciciel;
+			this.saldo = saldo;
+		}
+
+		public override string ToString()
+		{
+			return $"Konto : właściciel {wlasciciel}, saldo: {saldo}";
+		}
+
+		public virtual void Wyplata(int wyplata)
+		{
+			this.saldo = wyplata; 
+		}
+	}
 }
--- sprawdzian_probny/KontoFirmowe.cs
+++ sprawdzian_probny/KontoFirmowe.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace sprawdzian_probny
 {
-    class KontoFirmowe
+    class KontoFirmowe : Konto
     {
+        public KontoFirmowe(string wlasciciel, int saldo) : base(wlasciciel, saldo)
+        {
+             
+        }
+
+        public override string ToString()
+        {
+            return $"Konto firmmowe : właściciel {this.Wlasciciel}, saldo:{this.Saldo}";
+        }
+
+        public override void Wyplata(int wyplata)
+        {
+            base.Wyplata(wyplata);
+            this.Saldo -= 10;
+        }
     }
 }
